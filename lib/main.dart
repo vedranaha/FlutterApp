877c3f0db05ef2595af6e6557a363d40bf78d00b
Vedranaha <vedranaha@gmail.com>
2022-01-28 21:52:45 +0100

    no animation

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange[200],
+      backgroundColor: Colors.orange[100],
       body: const Center(
         child: SizedBox(
           width: 400,
@@ -46,8 +46,6 @@ class _SignUpFormState extends State<SignUpForm> {
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
-  double _formProgress = 0;
-
   void formProgress() {
     var progress = 0.0;
     final controllers = [
@@ -63,7 +61,7 @@ class _SignUpFormState extends State<SignUpForm> {
     }
 
     setState(() {
-      _formProgress = progress;
+      progress = progress;
     });
   }
 
@@ -73,8 +71,7 @@ class _SignUpFormState extends State<SignUpForm> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          AnimatedProgressIndicator(value: _formProgress),
-          Text('Sign up', style: Theme.of(context).textTheme.headline4),
+          Text('Welcome user', style: Theme.of(context).textTheme.headline3),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextFormField(
@@ -136,71 +133,6 @@ class _SignUpFormState extends State<SignUpForm> {
             child: const Text('Sign up'),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class AnimatedProgressIndicator extends StatefulWidget {
-  final double value;
-
-  const AnimatedProgressIndicator({
-    required this.value,
-  });
-
-  @override
-  State<StatefulWidget> createState() {
-    return _AnimatedProgressIndicatorState();
-  }
-}
-
-class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorAnimation;
-  late Animation<double> _curveAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1200),
-      vsync: this,
-    );
-
-    final colorTween = TweenSequence([
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.red, end: Colors.orange),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.yellow, end: Colors.green),
-        weight: 1,
-      ),
-    ]);
-
-    _colorAnimation = _controller.drive(colorTween);
-    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _controller.animateTo(widget.value);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) => LinearProgressIndicator(
-        value: _curveAnimation.value,
-        valueColor: _colorAnimation,
-        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
       ),
     );
   }
